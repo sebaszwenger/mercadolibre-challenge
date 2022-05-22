fcfe1a5ce3568fd5f083750ed703e0b85f8cb456
sebaszwenger <sebaszwenger@gmail.com>
2022-05-21 21:23:27 -0300

    add page Product. modify some routes. add NoMatch Page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import Search from "./pages/Search";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
@@ -9,8 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/items?search=" element={<Search />} />
-        <Route path="/items/:id" element={<Home />} />
+        <Route path="/items/" element={<Search />} />
+        <Route path="/items/:id" element={<Product />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
   );
