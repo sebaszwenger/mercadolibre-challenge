615b2373c90b038a25c726832bf807667b31fbba
sebaszwenger <sebaszwenger@gmail.com>
2022-05-21 21:30:35 -0300

    added a LogoMeli component

---
+++ src/components/LogoMeli.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LogoMeli = () => {
+  return (
+    <img
+      src="/img/mercadolibre-curso.png"
+      alt="logo mercadolibre"
+      width={40}
+      height={40}
+      className="mr-3"
+    />
+  );
+};
+
+export default LogoMeli;
