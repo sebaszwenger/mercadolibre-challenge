88b6620ed98073f13f62bc72e645c8a555b2cca6
sebaszwenger <sebaszwenger@gmail.com>
2022-05-23 12:53:45 -0300

    add a custom Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-const Button = (props) => {
+export const Button = ({ text, ...props }) => {
   return (
     <button className={props.className} onClick={props.onClick}>
-      {props.value}
+      {text}
     </button>
   );
 };
 
-export default Button;
+export const YellowButton = (props) => {
+  return (
+    <Button
+      {...props}
+      className={`${props.className}
+        " bg-mercadolibre shadow-gray-400 shadow-sm rounded-md`}
+    />
+  );
+};
+
+export const BuyButton = (props) => {
+  return (
+    <Button
+      {...props}
+      text={"Comprar"}
+      className={`${props.className} 
+        " bg-blue-500 text-white rounded-sm shadow shadow-blue-800 w-full`}
+    />
+  );
+};
