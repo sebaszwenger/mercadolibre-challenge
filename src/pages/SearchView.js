c8e447a16f13c00626df5082f5adf17c36017686
sebaszwenger <sebaszwenger@gmail.com>
2022-05-23 12:54:34 -0300

    change html button for a custom YellowButton

--- src/pages/SearchView.js
+++ src/pages/SearchView.js
@@ -1,27 +1,25 @@
 import React from "react";
-import Button from "../components/Button";
+import { YellowButton } from "../components/Button";
 import ListProducts from "../components/ListProducts";
 
-const SearchView = ({ data, handlePrevPage, handleNextPage }) => {
+const SearchView = ({ data, handlePrevPage, handleNextPage, page }) => {
   return (
     <div className="w-full h-full py-10 flex flex-col justify-center">
       <ListProducts data={data?.results} />
       <div className="flex justify-center gap-5 pt-5 pb-10">
-        <Button
-          value={"Previous"}
-          onClick={handlePrevPage}
-          handlePrevPage={handlePrevPage}
-          className={
-            "bg-mercadolibre px-2 py-1 shadow-gray-400 shadow-sm rounded-md"
-          }
-        />
-        <Button
-          value={"Next"}
+        {page > 0 && (
+          <YellowButton
+            text={"Previous"}
+            onClick={handlePrevPage}
+            handlePrevPage={handlePrevPage}
+            className={"py-1 px-2"}
+          />
+        )}
+        <YellowButton
+          text={"Next"}
           onClick={handleNextPage}
           handleNextPage={handleNextPage}
-          className={
-            "bg-mercadolibre px-2 py-1 shadow-gray-400 shadow-sm rounded-md"
-          }
+          className={"py-1 px-2"}
         />
       </div>
       )
